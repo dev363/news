763d0504cf0bcada4c8e96a59352a834d8fd2a71
satn363 <satn363@gmail.com>
2019-09-06 21:07:27 +0530

    change urls

--- src/components/includes/header.js
+++ src/components/includes/header.js
@@ -8,17 +8,17 @@ export default function Header(){
           <div className="container-fluid">
             <div className="navbar-header">
               <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span className="sr-only">Toggle navigation</span> <span className="icon-bar"></span> <span className="icon-bar"></span> <span className="icon-bar"></span> </button>
-              <Link className="navbar-brand" to="/"><img src="../logo.png" className="logo"/></Link> </div>
+              <Link className="navbar-brand" to="/"><img src="../logo.png" className="logo"/></a> </div>
             <div id="navbar" className="navbar-collapse collapse">
               <ul className="nav navbar-nav custom_nav">
                 <li className=""><a href="/">Home</a></li>
-                <li><Link to="/c/business">Business</Link></li>
-                <li><Link to="/c/entertainment">Entertainment</Link></li>
-                <li><Link to="/c/general">General</Link></li>
-                <li><Link to="/c/health">Health</Link></li>
-                <li><Link to="/c/science">Science</Link></li>
-                <li><Link to="/c/sports">Sports</Link></li>
-                <li><Link to="/c/technology">Technology</Link></li>              
+                <li><a href="/c/business">Business</a></li>
+                <li><a href="/c/entertainment">Entertainment</a></li>
+                <li><a href="/c/general">General</a></li>
+                <li><a href="/c/health">Health</a></li>
+                <li><a href="/c/science">Science</a></li>
+                <li><a href="/c/sports">Sports</a></li>
+                <li><a href="/c/technology">Technology</a></li>              
               </ul>
             </div>
           </div>
